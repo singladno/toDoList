3f0ced155808d8d9fe3081ba5a9e877bdac01e7a
singladno <singladno@gmail.com>
2019-10-14 15:59:43 +0300

    a

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState, createRef } from 'react'
 import ReactDOM from 'react-dom'
 import { Table } from 'reactstrap'
 import moment, { locale, locales } from 'moment'
+import { Button } from 'reactstrap'
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const weekdays = moment.weekdaysShort()
 const hours = []
@@ -15,10 +18,24 @@ for (var i = 0; i < 24; i++) {
 // // td.innerHTML = 'aaa'
 
 export default function Calendar() {
+    const [startOfWeek, setStartOfWeek] = useState(moment().startOf('week').add(1, 'days').format('DD'))
+    
+
+    function toggleWeekBack() {
+        setStartOfWeek(startOfWeek => {
+          return moment(startOfWeek, "DD").subtract(1, 'week').format("DD")
+        })
+    }
+    
+    function toggleWeekForward() {
+        setStartOfWeek(startOfWeek => {
+            return moment(startOfWeek, "DD").add(1, 'week').format("DD")
+          })
+    }
 
     useEffect(() => {
+        const today = moment().format("ddd")
         let works = JSON.parse(localStorage.getItem('works'))
-        console.log(works)
         works.forEach(item => {
             let day = moment(item.date).format("ddd")
             let time = item.time.toString();
@@ -28,11 +45,17 @@ export default function Calendar() {
             time = time.substr(0, time.indexOf(':')).concat(":00")
             let el =  document.getElementsByClassName(`${day} ${time}`);
             el[0].innerHTML = item.title
+            let thDay = document.querySelector(`th.${today}`)
+            thDay.classList.add("current")
         });
     })
 
     return (
         <div className="Calendar">
+            <div className="toggleWeek">
+                <Button onClick={toggleWeekBack} style={{marginRight:'10px'}}color="link" size="lg"><IoIosArrowBack/></Button>
+                <Button onClick={toggleWeekForward} color="link" size="lg"><IoIosArrowForward/></Button>
+            </div>
             <Table bordered>
                 <thead>
                     <tr className="weekDays">
@@ -42,11 +65,11 @@ export default function Calendar() {
                             {weekdays.map((day, index) => {
                                 if (index > 0) {
                                     return (
-                                        <th>{day}</th>
+                                        <th className={day}>{`${day}, `} <span className="num">{`${moment(startOfWeek, "DD").add(index - 1, "day").format("DD")}`}</span></th>
                                     )
                                 }
                             })}
-                            <th>{weekdays[0]}</th>
+                            <th>{`${weekdays[0]}, ${moment(startOfWeek, "DD").add(6, "days").format("DD")}`}</th>
                     </tr>
                 </thead>
 
@@ -55,13 +78,13 @@ export default function Calendar() {
                     return (
                         <tr>
                             <td>{hour}</td>
-                            <td className={`Mon ${hour} cell`}></td>
-                            <td className={`Tue ${hour} cell`}></td>
-                            <td className={`Wed ${hour} cell`}></td>
-                            <td className={`Thu ${hour} cell`}></td>
-                            <td className={`Fri ${hour} cell`}></td>
-                            <td className={`Sat ${hour} cell`}></td>
-                            <td className={`Sun ${hour} cell`}></td>
+                            <td className={`Mon ${moment(startOfWeek, "DD").format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Tue ${moment(startOfWeek, "DD").add(1, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Wed ${moment(startOfWeek, "DD").add(2, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Thu ${moment(startOfWeek, "DD").add(3, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Fri ${moment(startOfWeek, "DD").add(4, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Sat ${moment(startOfWeek, "DD").add(5, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
+                            <td className={`Sun ${moment(startOfWeek, "DD").add(6, 'days').format("DD MM YYYY")} ${hour} cell`}></td>
                         </tr>
                     )
                 })}
